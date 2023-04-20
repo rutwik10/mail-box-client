126200633791c5ccd3e4e88d7600523f86f07fda
rutwik10 <rutwikgosavi@gmail.com>
2023-04-20 09:50:23 +0530

    first commit

---
+++ src/Components/Routes/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        margin: "15%",
+       
+        fontSize: "90px",
+        color: "blue",
+      }}
+    >
+      Welcome to your mail box !!!
+    </div>
+  );
+};
+
+export default Home;
